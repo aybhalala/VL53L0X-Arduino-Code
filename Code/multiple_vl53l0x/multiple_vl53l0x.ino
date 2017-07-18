021c11371745f8debb56f19df7301a12961021a1
Aditya Patel <aybhalala@gmail.com>
2017-07-18 18:33:43 -0400

    dded files

---
+++ Code/multiple_vl53l0x/multiple_vl53l0x.ino
@@ -0,0 +1,65 @@
+/*
+============================================================================
+Created by Aditya Patel
+Email: me@pateladitya.com
+================================================================================
+*/
+#include <Wire.h>
+#include <VL53L0X.h>
+
+#define XSHUT_pin1 5 //connect sensor 1 XSHUT pin to arduino D5
+#define XSHUT_pin2 6 //connect sensor 2 XSHUT pin to arduino D6
+#define XSHUT_pin3 7 //connect sensor 3 XSHUT pin to arduino D7
+
+#define Sensor1_newAddress 42
+#define Sensor2_newAddress 43
+#define Sensor3_newAddress 45
+
+VL53L0X Sensor1;
+VL53L0X Sensor2;
+VL53L0X Sensor3;
+
+
+void setup() {
+ pinMode(XSHUT_pin1, OUTPUT);
+ pinMode(XSHUT_pin2, OUTPUT);
+ pinMode(XSHUT_pin3, OUTPUT);
+
+ Serial.begin(9600);
+ Wire.begin();
+
+ pinMode(XSHUT_pin1, INPUT);
+ delay(10);
+ Sensor1.setAddress(Sensor1_newAddress);
+ pinMode(XSHUT_pin2, INPUT);
+ delay(10);
+ Sensor2.setAddress(Sensor2_newAddress);
+ pinMode(XSHUT_pin3, INPUT);
+ delay(10);
+ Sensor3.setAddress(Sensor3_newAddress);
+ delay(10);
+
+ Sensor1.init();
+ Sensor2.init();
+ Sensor3.init();
+ Sensor1.setTimeout(500);
+ Sensor2.setTimeout(500);
+ Sensor3.setTimeout(500);
+ Sensor1.startContinuous();
+ Sensor2.startContinuous();
+ Sensor3.startContinuous();
+
+}
+
+void loop() {
+  Serial.print(Sensor1.readRangeContinuousMillimeters());
+  Serial.println("    sensor 1");
+  delay(500);
+  Serial.print(Sensor2.readRangeContinuousMillimeters());
+  Serial.println("    sensor 2");
+  delay(500);
+  Serial.print(Sensor3.readRangeContinuousMillimeters());
+  Serial.println("    sensor 3");
+  delay(500);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # VL53L0X-Arduino-Code
+
+This code allows you to use multiple VL53L0X sensors to one Arduino Board using a single I2C port.
+
+# Connections
+Connect all the VL53LOX's SDA pins to the SDA of Arduino and all SCL to SCL of Arduino.
+Connect the XSHUT pin to respective pins as in code.
+Connect all the sensors to 5V.
+
+# Library
+To doenload the VL53L0X library, visit https://github.com/pololu/vl53l0x-arduino
